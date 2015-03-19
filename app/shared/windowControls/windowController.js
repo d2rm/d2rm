330e15b9b1e71ab466053a43c46700099038c200
Pavel Yarmak <pyarmak@users.noreply.github.com>
2015-03-19 19:55:12 -0400

    Save and restore window location on app close/start

--- app/shared/windowControls/windowController.js
+++ app/shared/windowControls/windowController.js
@@ -1,10 +1,20 @@
-app.controller('windowController', function ($scope) {
+app.controller('windowController', function ($scope, storageService) {
     var gui = require('nw.gui'),
-        win = gui.Window.get();
+        win = gui.Window.get(),
+        storage = storageService;
+
+    win.x = storage.get("windowLocationX");
+    win.y = storage.get("windowLocationY");
+
+    function saveWindowLocationOnScreen () {
+        storage.set("windowLocationX", win.x);
+        storage.set("windowLocationY", win.y);
+    }
 
     $scope.isWindowExpanded = false;
 
     $scope.closeWindow = function () {
+        saveWindowLocationOnScreen();
         win.close();
     };
     $scope.minimizeWindow = function () {
