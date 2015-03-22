5391e7c8a6d4b0346ec99a2d7bfc201f6188f297
Pavel Yarmak <pyarmak@users.noreply.github.com>
2015-03-22 01:47:30 -0400

    Added protractor e2e test support. Organized spec folder.

---
+++ protractor.js
@@ -0,0 +1,19 @@
+exports.config = {
+  chromeDriver: 'cache/0.12.0/win64/chromedriver',
+  directConnect: true,
+
+  // Capabilities to be passed to the webdriver instance.
+  capabilities: {
+    'browserName': 'chrome'
+  },
+
+  // Spec patterns are relative to the current working directly when
+  // protractor is called.
+  specs: ['spec/e2e/**/*.js'],
+
+  // Options to be passed to Jasmine-node.
+  jasmineNodeOpts: {
+    showColors: true,
+    defaultTimeoutInterval: 30000
+  }
+};
