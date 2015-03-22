37193fe61e24f31d6f13010c69e720eb57edf5bb
Pavel Yarmak <pyarmak@users.noreply.github.com>
2015-03-21 22:04:42 -0400

    Cleaned up playersController.

--- app/components/players/playersController.js
+++ app/components/players/playersController.js
@@ -1,14 +1,5 @@
-app.controller('featuredController', function ($scope, apiService) {
-    $scope.artist = {
-        id : null,
-        imageCover : null,
-        imageSmall : null,
-        name : null,
-        albums : []
+app.controller('playersController', function ($scope, apiService) {
+    $scope.player = {
+        id : null
     };
-
-
-    apiService.getArtistWithFullAlbum("Katy Perry", function (data) {
-        $scope.artist = data;
-    });
 });
