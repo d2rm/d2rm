4cee80f9f3a6fb403f9a68d95e83ff49f689051d
Pavel Yarmak <pyarmak@users.noreply.github.com>
2015-03-22 17:32:58 -0400

    Final setup for protractor e2e testing

--- protractor.js
+++ protractor.js
@@ -1,19 +1,21 @@
+'use strict';
+
 exports.config = {
-  chromeDriver: 'cache/0.12.0/win64/chromedriver',
-  directConnect: true,
+    chromeDriver: './cache/0.12.0/win64/chromedriver', // relative path to node-webkit's chromedriver
+    chromeOnly: true, // starting Selenium server isn't required in our case
+    specs: ['spec/e2e/**/*.js'],
+    baseUrl: 'file:///D:/Code/D2RM-Angular/index.html?test=protractor',
 
-  // Capabilities to be passed to the webdriver instance.
-  capabilities: {
-    'browserName': 'chrome'
-  },
+    onPrepare: function() {
 
-  // Spec patterns are relative to the current working directly when
-  // protractor is called.
-  specs: ['spec/e2e/**/*.js'],
+        // By default, Protractor use data:text/html,<html></html> as resetUrl, but 
+        // location.replace (see http://git.io/tvdSIQ) from the data: to the file: protocol is not allowed
+        // (we'll get ‘not allowed local resource’ error), so we replace resetUrl with one
+        // with the file: protocol (this particular one will open system's root folder)
+        browser.resetUrl = 'file:///D:/Code/D2RM-Angular/index.html';
 
-  // Options to be passed to Jasmine-node.
-  jasmineNodeOpts: {
-    showColors: true,
-    defaultTimeoutInterval: 30000
-  }
+        // This isn't required and used to avoid ‘Cannot extract package’ error showed
+        // before Protractor have redirected node-webkit to resetUrl.
+        browser.driver.get('file:///D:/Code/D2RM-Angular/index.html');
+    }
 };
