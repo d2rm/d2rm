46470fab5bbf0e48ecd2e7dc29de6a61f977e0f4
Pavel Yarmak <pyarmak@users.noreply.github.com>
2015-03-22 00:17:59 -0400

    Cleaned up playlist controller

--- app/components/playlist/playlistController.js
+++ app/components/playlist/playlistController.js
@@ -1,7 +1,3 @@
 app.controller('playlistController', function ($scope, $routeParams, DBService) {
-    $scope.playlistTracks = [];
 
-    DBService.getAllPlaylistTracks($routeParams.id, function (data) {
-        $scope.playlistTracks = data;
-    });
 });
