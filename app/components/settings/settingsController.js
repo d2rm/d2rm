840f9145308bf8374a2256bd37b654824d961973
Pavel Yarmak <pyarmak@users.noreply.github.com>
2015-04-05 20:24:15 -0400

    Discard changes to settings if save isn't pressed

--- app/components/settings/settingsController.js
+++ app/components/settings/settingsController.js
@@ -5,13 +5,14 @@ app.controller('settingsController', function ($scope, settingsService) {
 
     this.languages = require('./languages/metadata.json').languages;
 
-    this.model = settingsService.settings;
+    this.model = angular.copy(settingsService.settings);
 
     this.getAPIKey = function() {
         gui.Shell.openExternal('http://steamcommunity.com/dev/apikey');
     };
 
     this.save = function() {
+        settingsService.settings = this.model;
         settingsService.save();
         history.back();
     };
